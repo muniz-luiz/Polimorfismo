fcaaf031859fa039f7ac984da7e42d98f4fd7e3c
Luiz Muniz <eusouluizeduardomuniz@gmail.com>
2023-02-13 09:41:12 -0300

    Iniciando um pequeno projeto para entender polimorfismo de maneira definitiva

---
+++ Polimorfismo/Entities/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfismo.Entities
+{
+    internal class Employee
+    {
+        public string Name { get; set; }
+        public int Hours { get; set; }
+        public double valuePerHour { get; set; }
+    }
+}
